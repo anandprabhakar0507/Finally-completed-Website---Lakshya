e74b9301ea1722b9e75c2a28e8cb1c937da2f9a2
Anand prabhakar <anandprabhakar0507@gmail.com>
2020-11-08 01:56:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,30 +2,24 @@
 
 
 document.addEventListener("contextmenu", function(e){
-    alert("Right click is disabled for safety..");
     e.preventDefault();
 
 }, false);
 
 document.onkeydown = function(e) {
     if(event.keyCode == 123) {
-        alert("you are not allowed to do so.");
         return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        alert("you are not allowed to do so.");
         return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        alert("you are not allowed to do so.");
         return false;
     }
     if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        alert("you are not allowed to do so.");
         return false;
     }
     if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        alert("you are not allowed to do so.");
         return false;
     }
   }
